2f1074a79584c7fe98b0b41f1aef0ca221afe8ce
Alex Yu <alexy4744@hstat.org>
2019-04-04 19:57:35 +0000

    fix navbar and grid system on info

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 $(function() {
   const nav = $("nav");
+  const navDropdown = $("#nav-dropdown");
+
+  $(window).scroll(function(scrollEvent) {
+    if (window.scrollY > 50) {
+      nav.addClass("navbar-white")
+    }
+    else if (!navDropdown.hasClass("in")) {
+      nav.removeClass("navbar-white")
+    }
+  });
 
   $(".navbar-toggle").click(function() {
-    if (nav.hasClass("navbar-dropdown-expanded")) {
-      nav.removeClass("navbar-dropdown-expanded")
-    } else {
-      nav.addClass("navbar-dropdown-expanded")
+    if (navDropdown.hasClass("navbar-white")) {
+      nav.removeClass("navbar-white")
+    }
+    else {
+      nav.addClass("navbar-white")
     }
   })
-})
+});
