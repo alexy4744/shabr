7ba8760b1cdca4ca68e84c50c4ce2d95ad9373e9
Alex Yu <alexy4744@hstat.org>
2019-04-04 21:12:45 +0000

    fix navbar toggler bug

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(function() {
   const nav = $("nav");
+  const navDropdown = $("#nav-dropdown")
 
   // If the user scrolled enough, then make the navbar not transparent
   $(window).scroll(function() {
     if (window.scrollY) {
       nav.addClass("navbar-white")
     }
-    else if (!$("#nav-dropdown").hasClass("in")) {
+    else if (!navDropdown.hasClass("in")) {
       nav.removeClass("navbar-white")
     }
   });
 
   $(".navbar-toggle").click(function() {
+    // dont allow it to delete the class when the dropdown hasnt fully expanded yet
+    if (navDropdown.hasClass("collapsing")) return;
+
     if (nav.hasClass("navbar-white") && !window.scrollY) {
       nav.removeClass("navbar-white")
     }
